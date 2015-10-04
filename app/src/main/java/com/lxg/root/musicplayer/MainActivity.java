22065d43712c77c1be87e1861fb9030bc8b9fd5e
Liuxigang <lxgvisual@163.com>
2015-10-04 15:46:06 +0800

    create a app for drawerLayout Test

---
+++ app/src/main/java/com/lxg/root/musicplayer/MainActivity.java
@@ -0,0 +1,77 @@
+package com.lxg.root.musicplayer;
+
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listview;
+    private RelativeLayout ll;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listview=(ListView)findViewById(R.id.listview);
+        ll=(RelativeLayout)findViewById(R.id.linearLayout);
+        BaseAdapter ba=new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 5;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                LinearLayout linearLayout=new LinearLayout(MainActivity.this);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+                linearLayout.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+                TextView TV=new TextView(MainActivity.this);
+                TV.setText("Item" + position + "");
+                TV.setTextSize(30);
+                linearLayout.addView(TV);
+                return linearLayout;
+            }
+        };
+        listview.setAdapter(ba);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
